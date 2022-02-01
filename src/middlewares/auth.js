d13e4ebf88e7faf9ab739c3338461585cf153979
assafvan <vanunuassaf@gmail.com>
2022-02-01 17:15:54 +0200

    auth middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../model/user");
+const JWT_SECRET = process.env.SECRET_KEY;
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const user = await userModel.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+
+    if (!user) {
+      throw new Error();
+    }
+
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (e) {
+    res.status(401).send({ error: "Please authenticate." });
+  }
+};
+
+module.exports = auth;
