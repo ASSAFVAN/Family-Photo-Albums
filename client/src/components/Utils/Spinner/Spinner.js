b27b79a72e6fbe19dcd25765d412cfdbe931760a
assafvan <vanunuassaf@gmail.com>
2022-02-02 11:54:49 +0200

    adding spinner

---
+++ client/src/components/Utils/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./spinner.css";
+
+export default function Spinner() {
+  return (
+    <div className="spinner">
+      <div className="circle1 circle"></div>
+      <div className="circle2 circle"></div>
+      <div className="circle3 circle"></div>
+    </div>
+  );
+}
