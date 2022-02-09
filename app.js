47b10aaebca0d2f8627fb04347d8fa495cbaefcc
assafvan <vanunuassaf@gmail.com>
2022-02-09 21:56:26 +0200

    appjs

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const cors = require("cors");
 const path = require("path");
 require("./src/db/mongoose");
 const userRouter = require("./src/routes/userRoutes");
+const albumRouter = require("./src/routes/albumRoutes");
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/api", userRouter); //user router
+app.use("/api", albumRouter); //album router
 
 const port = process.env.PORT || 5000;
 
 const publicPath = path.join(__dirname, "client/build");
-app.use(cors());
 app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,50 @@
 import "./App.css";
-
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
 import Header from "./components/Header/Header";
 import SignUp from "./components/SignUp/SignUp";
+import SignIn from "./components/SignIn/SignIn";
+import SignOut from "./components/SignOut/SignOut";
 import NotFound from "./components/NotFound/NotFound";
+import AlbumsList from "./components/AlbumsList/AlbumsList";
+import ShowAlbum from "./components/ShowAlbum/ShowAlbum";
+import AddNewAlbum from "./components/AddNewAlbum/AddNewAlbum";
+import AlbumImage from "./components/AlbumImage/AlbumImage";
 
 function App() {
+  const [token, setToken] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (token) {
+      console.log(token);
+
+      localStorage.setItem("token", JSON.stringify(token));
+    }
+    const tokenString = localStorage.getItem("token");
+    const userToken = JSON.parse(tokenString);
+    setToken(userToken);
+    // history.push("/albumslist");
+  }, [token]);
+
+  if (!token) {
+    return <SignIn setToken={setToken} />;
+  }
+
   return (
-    <div className="App">
+    <div>
       <BrowserRouter>
         <Header />
         <Switch>
           <Route path="/" exact component={Homepage} />
-          <Route Path="/signup" exact component={SignUp} />
+          <Route path="/signup" exact component={SignUp} />
+          <Route path="/signin" exact component={SignIn} />
+          <Route path="/signout" exact component={SignOut} />
+          <Route path="/albumslist" exact component={AlbumsList} />
+          <Route path="/showalbum/:id" exact component={ShowAlbum} />
+          <Route path="/albums/image" exact component={AlbumImage} />
+          <Route path="/albums/addalbum" exact component={AddNewAlbum} />
           <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
