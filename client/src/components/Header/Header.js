b2e95bed9ea1d91085bcc41575cb6a6a46bbc502
assafvan <vanunuassaf@gmail.com>
2022-02-09 21:56:00 +0200

    header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./header.css";
 
 export default function Header() {
+  const token = localStorage.getItem("token");
+  const userToken = JSON.parse(token);
+
   return (
-    <div>
-      <header>
-        <Link to="/">
-          <ul>HomePage</ul>
-        </Link>
-        <Link to="/signup">
-          <ul>signup</ul>
-        </Link>
-      </header>
-    </div>
+    <nav className="navbar">
+      <div className="navbar--left">
+        <Link to="/">HomePage</Link>
+        <Link to="/signup">signup</Link>
+        <Link to="/signin">signin</Link>
+        <Link to="/signout">signout</Link>
+      </div>
+    </nav>
   );
 }
