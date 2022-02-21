dec8fdba10a34defd214a5ecb23f1e53c461417f
assafvan <vanunuassaf@gmail.com>
2022-02-22 00:02:31 +0200

    try to fix heroku err

--- client/src/API/Api.js
+++ client/src/API/Api.js
@@ -3,7 +3,8 @@ import axios from "axios";
 let url = "http://localhost:5000/api";
 
 if (process.env.NODE_ENV === "production") {
-  url = "api";
+  // url = "api";
+  url = "https://assaf-family-photo-album.herokuapp.com/api";
 }
 export default axios.create({
   baseURL: url,
